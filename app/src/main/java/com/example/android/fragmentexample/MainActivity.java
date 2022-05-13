87ad6baf0d6e844fb4540f3bca5af620661f4a91
Overlord <tanvir1005@student.sust.edu>
2022-05-13 20:01:51 +0600

    1st commit

--- app/src/main/java/com/example/android/fragmentexample/MainActivity.java
+++ app/src/main/java/com/example/android/fragmentexample/MainActivity.java
@@ -16,8 +16,8 @@
 
 package com.example.android.fragmentexample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
